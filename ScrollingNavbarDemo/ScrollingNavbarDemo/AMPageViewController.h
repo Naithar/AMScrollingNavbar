26ed75453b1754ddeff0ada29d80c70a38d7db6b
Naithar <devias.naith@gmail.com>
2015-04-18 16:52:12 +0300

    page view controller example placeholder.

---
+++ ScrollingNavbarDemo/ScrollingNavbarDemo/AMPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  AMPageViewController.h
+//  ScrollingNavbarDemo
+//
+//  Created by Naithar on 18.04.15.
+//  Copyright (c) 2015 Andrea Mazzini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMPageViewController : UIViewController
+
+@end
